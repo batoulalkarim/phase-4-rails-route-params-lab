edc3f25fd32db9fcafed7f80db7d94dbf8da3693
Batoul Alkarim <batoulalkarim1@gmail.com>
2022-04-03 22:18:03 -0400

    done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,20 @@
 class StudentsController < ApplicationController
 
+
+  #this is how you let the user find a route depending on a query param. 
+  #it returns a list of all students whose first or last name match the query parameter.
   def index
-    students = Student.all
+    students = if params[:name]
+                 Student.by_name(params[:name])
+               else
+                 Student.all
+               end    
     render json: students
+  end
+
+  def show 
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
